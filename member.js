e8ed72c8402f757f251d693545566d69a2ad4f51
Prerit Gupta <75621370+Prerit007@users.noreply.github.com>
2023-07-18 11:00:06 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+    var member = {
+        name: 'John Doe',
+        age: 30,
+        address: '123 Main St',
+        skills: ['javascript', 'html', 'css']
+    };
+    return member;
+}
